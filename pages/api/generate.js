461ccd01eddf6f48f0c77ad9e047531eb0518b0c
erikreilly <erikreillythegreat@gmail.com>
2023-01-10 12:17:23 -0800

    webpage

---
+++ pages/api/generate.js
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = "Generate 5 different delicious vegan recipe ideas using some or all of the ingredients provided. Ensure each recipe is well balanced and includes protein and vegetables. For each recipe, include an ingredient list for that recipe and a concise, summarized set of step-by-step instructions formatted in a bulleted list. Dont include ingredients that aren't vegan even if they are listed in the ingredients provided. Ingredients:";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 1000,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
